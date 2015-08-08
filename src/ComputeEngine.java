354ce70d36d68551cca2bd8755e517e37177526b
fanifanu@gmail.com <fanifanu@gmail.com>
2015-08-07 22:48:06 -0500

    setting up the project

---
+++ src/ComputeEngine.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+/**
+*	An implimantation of a distributed compute engine
+*/
+public class ComputeEngine{
+	public static void main(String args[]){
+		System.out.println("Hello world");
+	}
+}
